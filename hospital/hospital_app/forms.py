95bd98a04904b1d5998199c2268c3efe547e3303
Arijus <arijus1990@gmail.com>
2021-11-28 20:57:48 +0200

    receptionist dashboard Timepicker added

--- hospital/hospital_app/forms.py
+++ hospital/hospital_app/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser, Appointment
 from django import forms
 from django.contrib.auth import get_user_model
+from datetime import datetime, timedelta
 User = get_user_model()
 
 
@@ -19,6 +20,33 @@ class CustomUserChangeForm(UserChangeForm):
         fields = ('email',)
 
 
+def working_hours():
+    start = datetime.strptime('08:00', '%H:%M')
+    end = datetime.strptime('20:00', '%H:%M')
+    seq = [start]
+
+    while seq[-1] < end:
+        start = start + timedelta(minutes=30)
+        seq.append(start)
+
+    time = []
+
+    for s in seq:
+        corrected = datetime.strftime(s, '%m/%d/%Y %H:%M')
+        time.append(corrected[-5:])
+
+    choices = []
+
+    for t in time:
+        one_tuple = (t, t)
+        choices.append(one_tuple)
+
+    return choices
+
+
+CHOICES = working_hours()
+
+
 class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
@@ -30,5 +58,5 @@ class AppointmentForm(forms.ModelForm):
             self.fields['patient'].queryset = User.objects.filter(user_type="P")
             self.fields['doctor'].queryset = User.objects.filter(user_type="D")
             self.fields["date"].label = "Date (YYYY-MM-DD)"
-            self.fields["time"].label = "Time 24 hr (HH:MM)"
+            self.fields["time"] = forms.ChoiceField(choices=CHOICES)
 
